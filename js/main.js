4e2de10b5ebd471ce2e92e43f28d809ce07a9dbc
Alena Zueva <aleo.zueva@gmail.com>
2020-01-20 16:03:04 +0700

    buger menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel();
+$(".owl1").owlCarousel({
+  margin: 10,
+  loop: true,
+  lazyLoad: true,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1
+    }
+  }
+});
+
+
+let burger = document.querySelector(".menu-button");
+let mainNav = document.querySelector(".header__navigation");
+
+burger.addEventListener("click", function() {
+  if (burger.classList.contains("menu-button--open")) {
+    burger.classList.remove("menu-button--open");
+    burger.classList.add("menu-button--close");
+    mainNav.classList.remove("header__navigation--close");
+    mainNav.classList.add("header__navigation--open");
+  } else {
+    burger.classList.remove("menu-button--close");
+    burger.classList.add("menu-button--open");
+    mainNav.classList.remove("header__navigation--open");
+    mainNav.classList.add("header__navigation--close");
+  }
 });
